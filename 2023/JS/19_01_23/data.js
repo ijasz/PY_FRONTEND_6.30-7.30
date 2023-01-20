0438ea18750db5ed49f6868e438fcc24076724ae
“ijasz” <jass07rtr@gmail.com>
2023-01-20 18:51:52 +0530

    added a file at 20/01/2023

---
+++ 2023/JS/19_01_23/data.js
@@ -0,0 +1,246 @@
+const students = [
+  {
+    name: "santhosh",
+    location: "pondy",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Ilakkiya",
+    location: "London",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Viknesh",
+    location: "Bangalore",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+];
---
+++ 2023/JS/19_01_23/index.js
@@ -0,0 +1,295 @@
+const doc = document;
+// const a = ["a", "b", "c"];
+// const c = ["a", "b", "c"];
+
+// const b = {
+//   name: "xxx",
+//   age: 10,
+//   location: "pondy",
+// };
+
+// console.log(Array.prototype);
+// console.log(Array.prototype);
+
+// for (let i of a) {
+//   console.log(i);
+// }
+
+// for (let i in b) {
+//   console.log(i);
+// }
+
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i++);
+// }
+
+// do {
+//   console.log(i++);
+// } while (i <= 10);
+
+const students = [
+  {
+    name: "santhosh",
+    location: "pondy",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Ilakkiya",
+    location: "London",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Viknesh",
+    location: "Bangalore",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+  {
+    name: "Pravin",
+    location: "Chennai",
+    skills: ["HTMl", "CSS", "JS", "PY", "DJANGO"],
+  },
+];
+
+const row = doc.querySelector(".row");
+
+for (const key of students) {
+  const card = doc.createElement("main");
+  const name = doc.createElement("p");
+  const location = doc.createElement("p");
+  const skills = doc.createElement("p");
+  name.textContent = `Name : ${key.name}`;
+  location.textContent = `Location : ${key.location}`;
+  skills.textContent = `Skills : ${key.skills}`;
+  card.className = "card";
+  card.append(name, location, skills);
+  row.appendChild(card);
+  //   console.log(row);
+}
+
+doc.body.appendChild(row);
