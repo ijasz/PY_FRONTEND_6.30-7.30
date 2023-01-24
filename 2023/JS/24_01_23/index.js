e9c3902c82d2d51778c516976e308a72ea308a3b
“ijasz” <jass07rtr@gmail.com>
2023-01-24 21:09:05 +0530

    added a file at 24/01/23

---
+++ 2023/JS/24_01_23/index.js
@@ -0,0 +1,26 @@
+// constructor function
+
+// function Person(fname, lname, age, location) {
+//   this.firstname = fname;
+//   this.lastname = lname;
+//   this.age = age;
+//   this.location = location;
+// }
+
+// Person.prototype.fullname = function () {
+//   return this.firstname + " " + this.lastname;
+// };
+
+// const obj1 = new Person("john", "smith", 10, "pondy");
+// const obj2 = new Person("santhosh", "smart", 20, "USA");
+
+// console.log(obj1);
+// console.log(obj2);
+
+// console.log(obj1.fullname());
+
+// IIFE
+
+// (function name(f, l) {
+//   console.log(f + " " + l);
+// })("bruno", "smith");
